3c61c6e1f6f44b2815fc7faca0eb55a41d4ea220
mawjngvux <vuvanmanh23062004@gmail.com>
2024-10-22 22:40:17 +0700

    Project

---
+++ message.py
@@ -0,0 +1,19 @@
+import telegram
+import asyncio
+
+my_token = "7701817287:AAGU0hQCP93I9799HD1p7Qjvjp240R_qMjc"
+bot = telegram.Bot(token = my_token)
+# chat_id = "6995112280"  # phamhanh
+chat_id = "7877025026"  #manhvu
+
+async def send_telegram(photo_path="alert.png"):
+    try:
+        await bot.send_photo(chat_id="7877025026", photo=open("test.jpg", "rb"), caption = "213")
+        # await bot.send_message(chat_id="7877025026", text="Yeu huong vch")
+
+    except Exception as ex:
+        print("Can not send message telegram ", ex)
+
+    print("Send sucess")
+
+asyncio.run(send_telegram("test.jpg"))
